8720c78c5ba8e525204c426446c41851d56eda0b
zzh <964389099@qq.com>
2022-09-19 09:36:51 +0800

    logback日志

--- src/main/java/com/sfbest/optimize/batch/controller/TestController.java
+++ src/main/java/com/sfbest/optimize/batch/controller/TestController.java
@@ -2,15 +2,9 @@ package com.sfbest.optimize.batch.controller;
 
 import com.sfbest.optimize.batch.mapper.db1.UserMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
